e4653bea5c60363ab079993eee1365125b3e2572
stevenliuyi <stevenliuyi@gmail.com>
2017-12-27 01:38:24 -0500

    use React-Table to render data table

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 class DataTable extends Component {
 
   render() {
     return (
-      <Table striped hover responsive>
-        <thead><tr>
-          { Array.isArray(this.props.header) &&
-            this.props.header.map((col, index) =>
-              (<th key={index}>{col}</th>))
-          }
-        </tr></thead>
-        <tbody>
-          { Array.isArray(this.props.data) &&
-            this.props.data.map((item, index) => (
-              <tr key={index}>
-                {
-                  this.props.header.map((col, index) => (
-                    <td key={index}>{ item[col] }</td>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </tbody>
-      </Table>
+      <div>
+        { Array.isArray(this.props.data) && this.props.data.length > 1 &&
+          <ReactTable
+            data={this.props.data}
+            filterable
+            columns={ this.props.header.map( col => {
+              return { Header: col, accessor: col }
+            })}
+            defaultPageSize={10}
+            defaultFilterMethod={(filter, row, column) => {
+              const id = filter.pivotId || filter.id
+              return row[id] !== undefined ?
+                String(row[id]).toLowerCase().includes(filter.value.toLowerCase()) :
+                true
+            }}
+            className='-striped -highlight'
+            pageSizeOptions={[10, 20, 50, 100, 200, 500, 1000, 2000, 5000]}
+          />
+        }
+      </div>
     )
   }
 }
