d30fe2051b48ff851cc9ff6c6ac3737d8cbe5aa5
stevenliuyi <stevenliuyi@gmail.com>
2018-02-21 01:26:18 -0500

    change message UI

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { Well } from 'react-bootstrap'
 import { info } from '../utils/info'
+import MdInfoOutline from 'react-icons/lib/md/info-outline'
+
+const iconStyle = {
+  verticalAlign: 'bottom'
+}
 
 class Info extends Component {
   render() {
@@ -13,10 +18,16 @@ class Info extends Component {
         : info[this.props.info].replace(/\[TEXT\]/, this.props.text)
 
     return (
-      <div>
-        {!showSettings && <Well className="info-text">{infoText}</Well>}
+      <div className="grey-text">
+        {!showSettings && (
+          <Well className="info-text">
+            <MdInfoOutline size={20} style={iconStyle} /> {infoText}
+          </Well>
+        )}
         {showSettings && (
-          <Well className="info-text show-settings">{infoText}</Well>
+          <Well className="info-text show-settings">
+            <MdInfoOutline size={20} style={iconStyle} /> {infoText}
+          </Well>
         )}
       </div>
     )
