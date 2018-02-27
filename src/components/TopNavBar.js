d23b0d8570bc9c6900dc67f692aec25fa19501b6
stevenliuyi <stevenliuyi@gmail.com>
2018-02-26 23:27:21 -0500

    responsive navbar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -14,32 +14,35 @@ class TopNavBar extends Component {
             </span>
             <span>Wikidata Visualization</span>
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
-        <Nav>
-          <NavItem
-            eventKey={1}
-            onSelect={() => this.props.handleChartSelect(1)}
-          >
-            Charts
-          </NavItem>
-          <NavItem
-            eventKey={2}
-            onSelect={() => this.props.handleChartSelect(2)}
-          >
-            Query Examples
-          </NavItem>
-          <NavItem
-            eventKey={3}
-            onSelect={() => this.props.handleChartSelect(3)}
-          >
-            About
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem href="https://github.com/stevenliuyi/wikidata-visualization">
-            <GoMarkGithub size={18} />
-          </NavItem>
-        </Nav>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem
+              eventKey={1}
+              onSelect={() => this.props.handleChartSelect(1)}
+            >
+              Charts
+            </NavItem>
+            <NavItem
+              eventKey={2}
+              onSelect={() => this.props.handleChartSelect(2)}
+            >
+              Query Examples
+            </NavItem>
+            <NavItem
+              eventKey={3}
+              onSelect={() => this.props.handleChartSelect(3)}
+            >
+              About
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem href="https://github.com/stevenliuyi/wikidata-visualization">
+              <GoMarkGithub size={18} />
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     )
   }
