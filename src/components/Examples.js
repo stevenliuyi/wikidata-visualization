e323ee3709932ed5cc3323ff532352baf61a8073
stevenliuyi <stevenliuyi@gmail.com>
2018-02-28 18:54:11 -0500

    query example table UI changes

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react'
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import { examples } from '../utils/examples'
+import MdSearch from 'react-icons/lib/md/search'
 
 class Examples extends Component {
+  filterComponent = ({ filter, onChange }) => (
+    <div className="pull-right">
+      <div style={{ position: 'absolute', marginLeft: '3px' }}>
+        <MdSearch
+          size={18}
+          color="#999"
+          style={{ verticalAlign: '-webkit-baseline-middle' }}
+        />
+      </div>
+      <input
+        type="text"
+        onChange={event => onChange(event.target.value)}
+        size="100px"
+        style={{ paddingLeft: '22px' }}
+      />
+    </div>
+  )
+
   getTrProps = (state, rowInfo) => {
     if (rowInfo) {
       return {
@@ -25,7 +44,13 @@ class Examples extends Component {
         data={examples.sort((a, b) => a.title.localeCompare(b.title))}
         sortable={false}
         filterable
-        columns={[{ Header: 'Wikidata Query Examples', accessor: 'title' }]}
+        columns={[
+          {
+            Header: () => <h4>Wikidata Query Examples</h4>,
+            accessor: 'title',
+            Filter: this.filterComponent
+          }
+        ]}
         defaultPageSize={10}
         defaultFilterMethod={(filter, row, column) => {
           const id = filter.pivotId || filter.id
@@ -37,7 +62,8 @@ class Examples extends Component {
         }}
         getTrProps={this.getTrProps}
         className="-highlight"
-        pageSizeOptions={[10]}
+        showPageSizeOptions={false}
+        noDataText="No query examples found"
       />
     )
   }
