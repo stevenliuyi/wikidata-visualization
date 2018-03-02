f9b73e1ab9c3d1dd32bdc76c4bfee7d8c9e6b2ea
stevenliuyi <stevenliuyi@gmail.com>
2018-03-02 05:35:38 -0500

    auto-deploy to toolforge

---
+++ travis/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ssh -i ~/.ssh/id_rsa stevenliuyi@login.tools.wmflabs.org << EOF
+  become dataviz
+  webservice stop
+  rm -rf ./wikidata-visualization
+  rm -rf ./public_html
+  mkdir ./public_html
+  git clone -b wmflabs https://$GITHUB_TOKEN@github.com/stevenliuyi/wikidata-visualization.git
+  mv ./wikidata-visualization/* ./public_html
+  rm -rf ./wikidata-visualization
+  webservice start
+  exit
+EOF
